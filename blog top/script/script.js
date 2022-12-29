f4ea07e40bd7f478d3d5dd8f80399050e2188ce6
Tsora1216 <79049713+Tsora1216@users.noreply.github.com>
2022-12-30 08:54:32 +0900

    update blog

--- blog top/script/script.js
+++ blog top/script/script.js
@@ -1,100 +1,23 @@
-// typewriter text
-const typewriter_text = ["Welcome To My Blog!!", "By Sora Takaya..."];
+// blog search
+const blogSearch = document.querySelector('#blogSearch');
+blogSearch.addEventListener("keyup", filterPosts);
 
-// responsive navigation
-const navSlide = () => {
-	const burger = document.querySelector ('.burger');
-	const nav = document.querySelector ('.nav-links');
-	const navlinks = document.querySelectorAll ('.nav-links li');
-
-	burger.addEventListener ('click', () => {
-		// toggle nav
-		nav.classList.toggle ('nav-active');
-	
-		// animate links
-		navlinks.forEach ((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navlinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-			}
-		});
-		
-		// animate burger
-		burger.classList.toggle ('toggle');
-	});
+function filterPosts() {
+  let filterValue = blogSearch.value.toLowerCase();
+  const posts = document.querySelectorAll(".card").forEach(post => {
+    post.innerText.toLowerCase().indexOf(filterValue)
+   > -1 ? post.style.display = '' : post.style.display = 'none';
+  })
 }
+// intersection observer
+const header = document.querySelector(".hero");
+const mainNav = document.querySelector(".mainNav");
 
-// sleep function
-function sleep(milliseconds) {
-    let timeStart = new Date().getTime();
-    while (true) {
-      	let elapsedTime = new Date().getTime() - timeStart;
-      	if (elapsedTime > milliseconds) {
-        	break;
-      	}
-    }
+const navObsCallback = (e) => {
+  !e[0].isIntersecting ? mainNav.classList.add("applyBackground") : mainNav.classList.remove("applyBackground");
 }
+const navObsOptions = {}
 
-let textIter = 0;
+const navObs = new IntersectionObserver(navObsCallback, navObsOptions);
 
-// typewriter effect - text deletion
-function deletingEffect () {
-	let word = typewriter_text[textIter].split ("");
-	var loopDeleting = function () {
-		if (word.length > 0) {
-			word.pop ();
-			document.getElementById ('typewriter-text').innerHTML = word.join ("");
-		} else {
-			if (typewriter_text.length > (textIter + 1)) {
-				textIter++;
-			} else {
-				textIter = 0;
-			}
-
-			typingEffect ();
-			return false;
-		}
-
-		setTimeout (loopDeleting, 150);
-	}
-
-	loopDeleting ();
-}
-
-// typewriter effect - text writing
-function typingEffect () {
-	let word = typewriter_text[textIter].split ("");
-	var loopTyping = function () {
-		if (word.length > 0) {
-			document.getElementById ('typewriter-text').innerHTML += word.shift();
-		} else {
-			sleep (1000);
-			deletingEffect ();
-			return false;
-		}
-
-		setTimeout (loopTyping, 150);
-	}
-
-	loopTyping ();
-}
-
-// scroll indicator
-function UpdateScrollProgress () {
-	var toppos = document.documentElement.scrollTop;
-	var remaining = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-	var percentage = (toppos / remaining) * 100;
-
-	document.getElementsByClassName ('progress-bar')[0].style.width = percentage + "%";
-}
-
-// scroll event listener
-document.addEventListener ("scroll", () => {
-	UpdateScrollProgress ();
-})
-
-// calling functions
-typingEffect ();
-navSlide ();
+navObs.observe(header);
